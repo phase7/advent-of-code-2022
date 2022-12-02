80fcd1557d6bdd1dab1a2adb6a4b3593c0128744
Ratul R <ratulroy20@gmail.com>
2022-12-03 00:32:17 +0600

    linter fix

--- src/day-2/rps.py
+++ src/day-2/rps.py
@@ -6,19 +6,16 @@ def checker(instance: str, cases: set[str]) -> bool:
     return True if instance in cases else False
 
 
-WIN_CASES = {'A Y', 'B Z', 'C X'}
+WIN_CASES = {"A Y", "B Z", "C X"}
 does_win = partial(checker, cases=WIN_CASES)
 
-DRAW_CASES = {'A X', 'B Y', 'C Z'}
+DRAW_CASES = {"A X", "B Y", "C Z"}
 is_draw = partial(checker, cases=DRAW_CASES)
 
 
 @cache
 def scorer(instance: str) -> int:
-    shape_score = dict(
-        # A=1, B=2, C=3,
-        X=1, Y=2, Z=3
-    )
+    shape_score = dict(X=1, Y=2, Z=3)
     score = shape_score[instance[-1]]
     if does_win(instance):
         score += 6
@@ -27,17 +24,17 @@ def scorer(instance: str) -> int:
     return score
 
 
-def total_score_puzzle1(cases=['A Y', 'B X', 'C Z']):
+def total_score_puzzle1(cases=["A Y", "B X", "C Z"]):
     scores = map(scorer, cases)
     return sum(scores)
 
 
-def total_score_puzzle2(cases=['A Y', 'B X', 'C Z']):
-    '''
+def total_score_puzzle2(cases=["A Y", "B X", "C Z"]):
+    """
     X lose
     Y draw
     Z win
-    '''
+    """
     transform = {
         "A X": "A Z",
         "A Y": "A X",
@@ -47,7 +44,8 @@ def total_score_puzzle2(cases=['A Y', 'B X', 'C Z']):
         "B Z": "B Z",
         "C X": "C Y",
         "C Y": "C Z",
-        "C Z": "C X"}
+        "C Z": "C X",
+    }
     cases = map(transform.get, cases)
     scores = map(scorer, cases)
     return sum(scores)
